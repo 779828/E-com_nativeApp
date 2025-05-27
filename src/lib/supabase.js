7f8e8605ea42ae774114164bd893c8163e663bb4
Shashank <shashankbmusale07@gmail.com>
2025-05-27 16:40:39 +0530

    add .env

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,6 @@
 import "react-native-url-polyfill/auto";
 import { createClient } from "@supabase/supabase-js";
 
-supabaseUrl = "https://gngqdgiucmobnjzmoamx.supabase.co";
-supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduZ3FkZ2l1Y21vYm5qem1vYW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMDUzNTEsImV4cCI6MjA2Mjg4MTM1MX0.Nbt-YLwaj8pg45P5oaG6MGUBtsXp_7DhdCEyHLz-UdU";
+import { SUPABASE_URL, SUPABASE_KEY } from "@env";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
